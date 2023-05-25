8a61e325686a4115ea6381e9b7ce3200a9408750
YashsviG <girdhar.yashsvi@gmail.com>
2023-05-25 10:35:59 +0530

    cleaning u up the code

--- app/src/main/java/com/yashsvi/extractmanifest/MainActivity.java
+++ app/src/main/java/com/yashsvi/extractmanifest/MainActivity.java
@@ -1,6 +1,6 @@
 package com.yashsvi.extractmanifest;
 
-import android.content.ContentResolver;
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -10,19 +10,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.io.ByteArrayOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
-
-import androidx.documentfile.provider.DocumentFile;
-
 import java.io.File;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+/*
+ * These two imports are the libraries
+ * used extract the contents of the XML file.
+ */
+import org.xmlpull.v1.XmlPullParser;
+import brut.androlib.res.decoder.AXmlResourceParser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,104 +43,145 @@ public class MainActivity extends AppCompatActivity {
         selectApkButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                if (ContextCompat.checkSelfPermission(MainActivity.this,
-//                        Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-//                    ActivityCompat.requestPermissions(MainActivity.this,
-//                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-//                            REQUEST_CODE_PERMISSION);
-//                } else {
+                /* THIS IF CONDITION DOESN'T WOK ON EMULATOR PROPERLY */
+                /* PLEASE COMMENT THIS TO TEST ON EMULATOR */
+
+                /* Gets and checks for permission to read external storage */
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            REQUEST_CODE_PERMISSION);
+                } else {
                     pickApkFile();
-//                }
+                }
             }
         });
     }
 
+    /**
+     * Picks the APK file from the file explorer
+     */
     private void pickApkFile() {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("application/vnd.android.package-archive");
         startActivityForResult(intent, REQUEST_CODE_PICK_APK);
     }
 
+    /**
+     * Runs once the pickApkFile is completed
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == REQUEST_CODE_PICK_APK && resultCode == RESULT_OK && data != null) {
             Uri apkUri = data.getData();
+
+            /* if apkUri has data in it, calls the extractManifest function */
             if (apkUri != null) {
-                extractManifest(apkUri);
+                /* This is the actual file path */
+                String filePath = PathUtil.getPath(apkUri, this.getApplicationContext());
+                try {
+                    extractManifestXml(apkUri);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
     }
 
-    private void extractManifest(Uri contentUri) {
+    /**
+     * This utilizes apktool library to make the extracted manifest Content readable
+     */
+    private String extractManifestXml(Uri uri) throws IOException {
+        StringBuilder manifestXml = new StringBuilder();
+        InputStream inputStream = getContentResolver().openInputStream(uri);
         try {
-            InputStream inputStream = getContentResolver().openInputStream(contentUri);
-            if (inputStream != null) {
-                ZipInputStream zipInputStream = new ZipInputStream(inputStream);
-                ZipEntry zipEntry;
+            /*
+            Using the apktool AXMLResourceParser to parse
+            through the content of the AndroidManifest.xml
+             */
+            AXmlResourceParser parser = new AXmlResourceParser();
+            parser.open(inputStream);
 
-                while ((zipEntry = zipInputStream.getNextEntry()) != null) {
-                    if (zipEntry.getName().equals("AndroidManifest.xml")) {
-                        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-                        byte[] buffer = new byte[1024];
-                        int length;
-
-                        while ((length = zipInputStream.read(buffer)) > 0) {
-                            outputStream.write(buffer, 0, length);
-                        }
-
-                        String manifestContent = new String(outputStream.toByteArray());
-                        System.out.println(manifestContent);
-                        // Save manifestContent to a file
-                        String fileName = "manifest.xml";
-                        File outputDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-                        File outputFile = new File(outputDir, fileName);
-
-                        FileWriter fileWriter = new FileWriter(outputFile);
-                        fileWriter.write(manifestContent);
-                        fileWriter.close();
-
-                        // Display a Toast indicating the file creation status
-                        if (outputFile.exists()) {
-                            showToast("File created successfully!");
-                        } else {
-                            showToast("File creation failed.");
-                        }
-
-                        break;
-                    }
+            while (true) {
+                int eventType = parser.next();
+                if (eventType == XmlPullParser.END_DOCUMENT) {
+                    break;
                 }
 
-                zipInputStream.close();
-                inputStream.close();
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:
+                        String tagName = parser.getName();
+                        manifestXml.append("<").append(tagName);
+
+                        int namespaceCount = parser.getNamespaceCount(parser.getDepth());
+                        for (int i = 0; i < namespaceCount; i++) {
+                            String namespacePrefix = parser.getNamespacePrefix(i);
+                            String namespaceUri = parser.getNamespaceUri(i);
+                            manifestXml.append(" xmlns");
+                            if (namespacePrefix != null && !namespacePrefix.isEmpty()) {
+                                manifestXml.append(":").append(namespacePrefix);
+                            }
+                            manifestXml.append("=\"").append(namespaceUri).append("\"");
+                        }
+
+                        int attributeCount = parser.getAttributeCount();
+                        for (int i = 0; i < attributeCount; i++) {
+                            String attributeName = parser.getAttributeName(i);
+                            String attributeValue = parser.getAttributeValue(i);
+                            manifestXml.append(" ").append(attributeName).append("=\"").append(attributeValue).append("\"");
+                        }
+
+                        manifestXml.append(">");
+                        break;
+                    case XmlPullParser.END_TAG:
+                        manifestXml.append("</").append(parser.getName()).append(">");
+                        break;
+                    case XmlPullParser.TEXT:
+                        manifestXml.append(parser.getText());
+                        break;
+                }
             }
-        } catch (IOException e) {
+
+            parser.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
+        /*
+        Saving the content now in the manifest.xml
+        file in the Downloads directory
+        */
+        String fileName = "manifest.xml";
+        File outputDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File outputFile = new File(outputDir, fileName);
+
+        FileWriter fileWriter = new FileWriter(outputFile);
+        fileWriter.write(manifestXml.toString());
+        fileWriter.close();
+
+        /* Display a Toast indicating the file creation status */
+        if (outputFile.exists()) {
+            showToast("File created successfully!");
+        } else {
+            showToast("File creation failed.");
+        }
+
+        return manifestXml.toString();
     }
 
-
+    /**
+     * Displays the toast based on the file creation status
+     */
     private void showToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
-    private String getRealPathFromURI(Uri contentUri) {
-        String filePath = null;
-
-        // Check if the URI scheme is "content"
-        if (ContentResolver.SCHEME_CONTENT.equals(contentUri.getScheme())) {
-            // Try to obtain the DocumentFile from the content URI
-            DocumentFile documentFile = DocumentFile.fromSingleUri(this, contentUri);
-            if (documentFile != null && documentFile.isFile()) {
-                // Retrieve the file path from the DocumentFile
-                filePath = documentFile.getUri().getPath();
-            }
-        }
-
-        return filePath;
-    }
-
+    /**
+     * Called when the permissions are set by the user, i.e. allowed or denied
+     */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
